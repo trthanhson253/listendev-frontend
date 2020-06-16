28ed0aa6520961fef3fb6b282bc3c8817ecb2355
trthanhson253 <trthanhson253@gmail.com>
2020-06-16 13:10:11 -0400

    second

---
+++ src/build/precache-manifest.cc9447f272f7163dd998c3a1eb1b530c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9ea1b051f560245cf0a5765223d4b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d43df1819a9cc10138",
+    "url": "/static/css/main.1ed2943f.chunk.css"
+  },
+  {
+    "revision": "c2a651e8299713c8ef29",
+    "url": "/static/js/2.64f55506.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.64f55506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d43df1819a9cc10138",
+    "url": "/static/js/main.eb2f08c5.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "f8709909f12752fffb4d3e3618ffc73b",
+    "url": "/static/media/banner.f8709909.jpg"
+  }
+]);
